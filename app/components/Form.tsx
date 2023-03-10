3fa828eb97a7bd213fa68f787d8b2bfb26c22033
Pedro Tigre <pedrogtigre@gmail.com>
2023-03-10 15:15:27 -0300

    style: add style modifications

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -48,34 +48,34 @@ export default function Form() {
     };
 
     return (
-        <div className='flex flex-col gap-5'>
-            <div className='mx-auto mt-12 flex h-36 w-full items-center justify-center rounded-lg bg-slate-100 p-2 text-center'>
+        <div className='z-10 flex w-full max-w-lg flex-col gap-5'>
+            <div className='sm:mt mx-auto mt-12 flex h-36 w-full items-center justify-center rounded-lg bg-[#111828] bg-opacity-80 p-2 text-center text-white shadow-xl xl:mt-48'>
                 {(phrase && <p>{phrase}</p>) || <p>...</p>}
             </div>
             <form className='mx-auto w-full' onSubmit={handleSubmit}>
-                <div className='overflow-hidden rounded-lg border border-gray-300 bg-white p-3 shadow-sm focus-within:border-slate-500 focus-within:ring-1 focus-within:ring-slate-500'>
-                    <h1 className='mb-3  text-lg font-medium text-gray-700'>
-                        Gerador de frases motivacionais
-                    </h1>
+                <div className=' overflow-hidden rounded-lg bg-[#111828]  bg-opacity-80 px-3 py-5 shadow-xl backdrop-blur backdrop-filter'>
                     <textarea
                         rows={2}
                         name='description'
                         id='description'
-                        className='mb-2 block w-full resize-none border-0  p-2 py-1 placeholder-gray-500 focus:outline-slate-400 focus:ring-0 sm:text-sm'
+                        className='mb-4 block w-full resize-none rounded-md border-0 bg-[#202938] bg-opacity-80 px-3 py-2 text-white placeholder-gray-400 focus:outline-1 focus:outline-slate-900 sm:text-sm'
                         placeholder='Digite uma palavra...'
-                        value={word}
                         onChange={(e) => setWord(e.target.value)}
                     />
 
-                    <div className='flex items-center justify-between space-x-3 border-t border-gray-200 px-2 py-2 sm:px-3'>
-                        <div className='flex'>
-                            <button
-                                className='inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
-                                type='submit'
-                            >
-                                Gerar frase
-                            </button>
-                        </div>
+                    <div className='flex items-center justify-between space-x-3 border-t border-[#242b3b] px-2 pt-4 sm:px-3'>
+                        <button
+                            className='inline-flex items-center rounded-md border border-transparent bg-indigo-600 bg-opacity-80 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+                            type='submit'
+                        >
+                            Gerar frase
+                        </button>
+                        <button
+                            className='inline-flex items-center rounded-md border border-transparent bg-indigo-600 bg-opacity-80 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+                            type='submit'
+                        >
+                            Editar prompt
+                        </button>
                     </div>
                 </div>
             </form>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import styles from './page.module.css';
 import Form from './components/Form';
+import Image from 'next/image';
 
 export default function Home() {
     return (
         <main className={styles.main}>
             <Form />
+            <Image
+                src='/mountainbg.jpg'
+                alt='Mountain landscape'
+                fill
+                style={{ objectFit: 'cover' }}
+            />
         </main>
     );
 }
